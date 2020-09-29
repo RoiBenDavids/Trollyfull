9a756d7ffa020db5ebd257d45c1a9800282d9459
roi <bendaroi@gmail.com>
2020-09-29 12:09:58 +0300

    sdf

--- frontend/src/cmps/MainCmps/Map.jsx
+++ frontend/src/cmps/MainCmps/Map.jsx
@@ -3,8 +3,8 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import { utils } from '../../services/utils';
 
 
-// const API_KEY = 'AIzaSyBXIyfwpDtmz9fLAQI-MUqWuhQtc-GQYoo'
-const API_KEY = ''
+const API_KEY = 'AIzaSyBXIyfwpDtmz9fLAQI-MUqWuhQtc-GQYoo'
+// const API_KEY = ''
 
 class _MapContainer extends React.Component {
     state = {
